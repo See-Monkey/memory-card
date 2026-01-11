28b01e0d8354b2b5ec0f219ea060081f80c9e949
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-01-11 15:24:32 -0700

    Capitalize title. Fix e's with accents

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import "../styles/Header.css";
 export default function Header({ loading, score, maxScore, gameOver }) {
 	return (
 		<header>
-			<h1>Pokemon Memory Card Game</h1>
+			<h1>POK&eacute;MON MEMORY CARD GAME</h1>
 			<div id="scoreContainer" className={!gameOver ? "visible" : "hidden"}>
 				<h3>Score: {loading ? "" : score}</h3>
 				<h3>High Score: {loading ? "" : maxScore}</h3>
@@ -12,7 +12,7 @@ export default function Header({ loading, score, maxScore, gameOver }) {
 				id="headerMsg"
 				className={!loading && !gameOver ? "visible" : "hidden"}
 			>
-				Try to select all the Pokemon without picking the same one twice
+				Try to select all the Pok&eacute;mon without picking the same one twice
 			</p>
 		</header>
 	);
