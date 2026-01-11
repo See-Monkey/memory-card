b93c18896c731d7b4517c5a794b974fdaa17fce9
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-01-11 01:42:29 -0700

    Add useEffect hook to get data on load or new game

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import getPokemon from "./functions/getPokemon.js";
 
 export default function App() {
-	const data = getPokemon(2);
-	console.log(data);
+	const [loading, setLoading] = useState(true);
+	const [pokemonArray, setPokemon] = useState([]);
+	const [selected, setSelected] = useState([]);
+	const [score, setScore] = useState(0);
+	const [maxScore, setMaxScore] = useState(0);
+	const [gameOver, setGameOver] = useState(false);
+	const [gamesPlayed, setGamesPlayed] = useState(0);
+
+	useEffect(() => {
+		async function getData() {
+			const data = await getPokemon(10);
+			setPokemon(data);
+			setLoading(false);
+		}
+		getData();
+	}, [gamesPlayed]);
 }
---
+++ src/components/DisplayCards.jsx
@@ -0,0 +1,1 @@
+import "../styles/DisplayCards.css";
